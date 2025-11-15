cffe237243020664aeb93370445149bd64fb5d16
TheGeniusOfEternity <max06safin@yandex.ru>
2025-11-15 12:25:39 +0300

    feat(core): install dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,21 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    implementation("org.glassfish:jakarta.faces:4.0.0")
+    implementation("org.primefaces:primefaces:12.0.0")
+    implementation("org.icefaces:icefaces-ace:4.3.0")
+    implementation("org.hibernate.orm:hibernate-core:6.2.7.Final")
+    implementation("org.postgresql:postgresql:42.7.7")
+    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.9.0")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
